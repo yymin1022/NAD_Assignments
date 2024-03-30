0843f14cb428918a697bf0b22f2750d34845b993
yymin1022 <yymin1022@gmail.com>
2024-03-30 15:18:48 +0900

    Update Assignment 2 : Cleaned UDP Client

--- Assignment 2/UDPClient.go
+++ Assignment 2/UDPClient.go
@@ -4,27 +4,31 @@
 
 package main
 
-import ("bufio"; "fmt"; "net"; "os";)
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
 
 func main() {
-    serverName := "nsl2.cau.ac.kr"
-    serverPort := "12000"
+	serverName := "nsl2.cau.ac.kr"
+	serverPort := "12000"
 
-    pconn, _:= net.ListenPacket("udp", ":")
+	conn, _ := net.ListenPacket("udp", ":")
 
-    localAddr := pconn.LocalAddr().(*net.UDPAddr)
-    fmt.Printf("Client is running on port %d\n", localAddr.Port)
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	fmt.Printf("Client is running on port %d\n", localAddr.Port)
 
-    fmt.Printf("Input lowercase sentence: ")
-    input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	fmt.Printf("Input lowercase sentence: ")
+	input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 
-    server_addr, _ := net.ResolveUDPAddr("udp", serverName+":"+serverPort)
-    pconn.WriteTo([]byte(input), server_addr)
+	server_addr, _ := net.ResolveUDPAddr("udp", serverName+":"+serverPort)
+	conn.WriteTo([]byte(input), server_addr)
 
-    buffer := make([]byte, 1024)
-    pconn.ReadFrom(buffer)
-    fmt.Printf("Reply from server: %s", string(buffer))
+	buffer := make([]byte, 1024)
+	conn.ReadFrom(buffer)
+	fmt.Printf("Reply from server: %s", string(buffer))
 
-    pconn.Close()
+	conn.Close()
 }
-
