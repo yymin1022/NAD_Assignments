eaac0af09f83255fcb460910033a3e14a76952fd
yymin1022 <yymin1022@gmail.com>
2024-03-30 18:27:44 +0900

    Update Assignment 2 : Seperated Functions for UDPClient

--- Assignment 2/UDPClient.go
+++ Assignment 2/UDPClient.go
@@ -11,24 +11,48 @@ import (
 	"os"
 )
 
+const SERVER_NAME = "nsl2.cau.ac.kr"
+const SERVER_PORT = "12000"
+
 func main() {
-	serverName := "nsl2.cau.ac.kr"
-	serverPort := "12000"
+	conn := makeConnection()
 
-	conn, _ := net.ListenPacket("udp", ":")
-
-	localAddr := conn.LocalAddr().(*net.UDPAddr)
-	fmt.Printf("Client is running on port %d\n", localAddr.Port)
+	if conn == nil {
+		return
+	}
 
 	fmt.Printf("Input lowercase sentence: ")
 	input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 
-	server_addr, _ := net.ResolveUDPAddr("udp", serverName+":"+serverPort)
+	server_addr, _ := net.ResolveUDPAddr("udp", SERVER_NAME+":"+SERVER_PORT)
 	conn.WriteTo([]byte(input), server_addr)
 
 	buffer := make([]byte, 1024)
 	conn.ReadFrom(buffer)
 	fmt.Printf("Reply from server: %s", string(buffer))
 
-	conn.Close()
+	closeConnection(conn)
+}
+
+func makeConnection() net.PacketConn {
+	conn, err := net.ListenPacket("udp", ":")
+
+	if err != nil {
+		fmt.Printf("Error: %s", err.Error())
+		return nil
+	}
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	fmt.Printf("Client is running on port %d\n", localAddr.Port)
+
+	return conn
+}
+
+func closeConnection(conn net.PacketConn) {
+	if conn != nil {
+		err := conn.Close()
+		if err != nil {
+			fmt.Printf("Error: %s", err.Error())
+		}
+	}
 }
