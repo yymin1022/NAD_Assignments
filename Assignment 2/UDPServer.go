cf3698433863c9e917a87ba62478417ee4cccefa
yymin1022 <yymin1022@gmail.com>
2024-03-30 15:19:16 +0900

    Update Assignment 2 : Cleaned UDP Server

--- Assignment 2/UDPServer.go
+++ Assignment 2/UDPServer.go
@@ -4,20 +4,23 @@
 
 package main
 
-import ("bytes"; "fmt"; "net")
+import (
+	"bytes"
+	"fmt"
+	"net"
+)
 
 func main() {
-    serverPort := "12000"
+	serverPort := "12000"
 
-    pconn, _:= net.ListenPacket("udp", ":"+serverPort)
-    fmt.Printf("Server is ready to receive on port %s\n", serverPort)
+	conn, _ := net.ListenPacket("udp", ":"+serverPort)
+	fmt.Printf("Server is ready to receive on port %s\n", serverPort)
 
-    buffer := make([]byte, 1024)
+	buffer := make([]byte, 1024)
 
-    for {
-        count, r_addr, _:= pconn.ReadFrom(buffer)
-        fmt.Printf("UDP message from %s\n", r_addr.String())
-        pconn.WriteTo(bytes.ToUpper(buffer[:count]), r_addr)
-    }
+	for {
+		count, r_addr, _ := conn.ReadFrom(buffer)
+		fmt.Printf("UDP message from %s\n", r_addr.String())
+		conn.WriteTo(bytes.ToUpper(buffer[:count]), r_addr)
+	}
 }
-
