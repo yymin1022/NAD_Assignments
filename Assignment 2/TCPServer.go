d48a85e627743163eb8b437eed80389f6c765155
yymin1022 <yymin1022@gmail.com>
2024-03-30 15:17:39 +0900

    Update Assignment 2 : Cleaned TCP Server

--- Assignment 2/TCPServer.go
+++ Assignment 2/TCPServer.go
@@ -4,22 +4,25 @@
 
 package main
 
-import ("bytes"; "fmt"; "net")
+import (
+	"bytes"
+	"fmt"
+	"net"
+)
 
 func main() {
-    serverPort := "12000"
+	serverPort := "12000"
 
-    listener, _:= net.Listen("tcp", ":" + serverPort)
-    fmt.Printf("Server is ready to receive on port %s\n", serverPort)
+	listener, _ := net.Listen("tcp", ":"+serverPort)
+	fmt.Printf("Server is ready to receive on port %s\n", serverPort)
 
-    buffer := make([]byte, 1024)
+	buffer := make([]byte, 1024)
 
-    for {
-        conn, _:= listener.Accept()
-        fmt.Printf("Connection request from %s\n", conn.RemoteAddr().String())
-        count, _ := conn.Read(buffer)
-        conn.Write(bytes.ToUpper(buffer[:count]))
-        conn.Close()
-    }
+	for {
+		conn, _ := listener.Accept()
+		fmt.Printf("Connection request from %s\n", conn.RemoteAddr().String())
+		count, _ := conn.Read(buffer)
+		conn.Write(bytes.ToUpper(buffer[:count]))
+		conn.Close()
+	}
 }
-
