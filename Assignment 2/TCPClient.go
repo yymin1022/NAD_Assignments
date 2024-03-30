c2c4069829d094b908dcce75a57d45213abe3bf3
yymin1022 <yymin1022@gmail.com>
2024-03-30 15:16:14 +0900

    Update Assignment 2 : Cleaned TCP Client

--- Assignment 2/TCPClient.go
+++ Assignment 2/TCPClient.go
@@ -4,25 +4,29 @@
 
 package main
 
-import ("bufio"; "fmt"; "net"; "os")
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
 
 func main() {
+	serverName := "nsl2.cau.ac.kr"
+	serverPort := "12000"
 
-    serverName := "nsl2.cau.ac.kr"
-    serverPort := "12000"
+	conn, _ := net.Dial("tcp", serverName+":"+serverPort)
 
-    conn, _:= net.Dial("tcp", serverName+":"+serverPort)
+	localAddr := conn.LocalAddr().(*net.TCPAddr)
+	fmt.Printf("Client is running on port %d\n", localAddr.Port)
 
-    localAddr := conn.LocalAddr().(*net.TCPAddr)
-    fmt.Printf("Client is running on port %d\n", localAddr.Port)
+	fmt.Printf("Input lowercase sentence: ")
+	input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	conn.Write([]byte(input))
 
-    fmt.Printf("Input lowercase sentence: ")
-    input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-    conn.Write([]byte(input))
+	buffer := make([]byte, 1024)
+	conn.Read(buffer)
+	fmt.Printf("Reply from server: %s", string(buffer))
 
-    buffer := make([]byte, 1024)
-    conn.Read(buffer)
-    fmt.Printf("Reply from server: %s", string(buffer))
-
-    conn.Close()
+	conn.Close()
 }
