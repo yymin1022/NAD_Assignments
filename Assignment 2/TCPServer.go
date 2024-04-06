5b83bb9f7415f0f026fdf4fe0eb704778afdf197
yymin1022 <yymin1022@gmail.com>
2024-04-06 17:12:55 +0900

    Update Assignment 2 : Migrated TCP Server Features from UDP Server

--- Assignment 2/TCPServer.go
+++ Assignment 2/TCPServer.go
@@ -5,24 +5,87 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"net"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
 )
 
+const TCP_SERVER_PORT = "14094"
+
+var serverResponseCnt int
+var serverStartTime time.Time
+
 func main() {
-	serverPort := "12000"
+	serverListener := initServer()
+	if serverListener == nil {
+		fmt.Println("Error: Failed to Init Server")
+		return
+	}
 
-	listener, _ := net.Listen("tcp", ":"+serverPort)
-	fmt.Printf("Server is ready to receive on port %s\n", serverPort)
+	sigintHandler := make(chan os.Signal, 1)
+	signal.Notify(sigintHandler, syscall.SIGINT)
+	go func() {
+		<-sigintHandler
+		closeServer(serverListener)
+		os.Exit(0)
+	}()
 
-	buffer := make([]byte, 1024)
+	serverResponseCnt = 0
+	serverStartTime = time.Now()
+
+	requestBuffer := make([]byte, 1024)
 
 	for {
-		conn, _ := listener.Accept()
-		fmt.Printf("Connection request from %s\n", conn.RemoteAddr().String())
-		count, _ := conn.Read(buffer)
-		conn.Write(bytes.ToUpper(buffer[:count]))
-		conn.Close()
+		serverConnection, _ := serverListener.Accept()
+		fmt.Printf("TCP Connection Request from %s\n", serverConnection.RemoteAddr().String())
+
+		count, _ := serverConnection.Read(requestBuffer)
+		responseData := getResponse(int(requestBuffer[0]), string(requestBuffer[1:count]), serverConnection.RemoteAddr().String())
+		_, err := serverConnection.Write([]byte(responseData))
+		if err != nil {
+			fmt.Println("Error: Failed to Send Response")
+			continue
+		}
+		serverResponseCnt++
+		_ = serverConnection.Close()
 	}
+}
+
+func initServer() net.Listener {
+	serverListener, err := net.Listen("tcp", ":"+TCP_SERVER_PORT)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err.Error())
+		return nil
+	}
+
+	fmt.Printf("Server is ready to receive on port %s\n", TCP_SERVER_PORT)
+
+	return serverListener
+}
+
+func closeServer(listener net.Listener) {
+	fmt.Println("\rClosing Server Program...\nBye bye~")
+	if listener != nil {
+		_ = listener.Close()
+	}
+}
+
+func getResponse(cmd int, data string, addr string) string {
+	switch cmd {
+	case 1:
+		return strings.ToUpper(data)
+	case 2:
+		curTime := time.Since(serverStartTime)
+		return fmt.Sprintf("run time = %02.0f:%02.0f:%02.0f", curTime.Hours(), curTime.Minutes(), curTime.Seconds())
+	case 3:
+		addrInfo := strings.Split(addr, ":")
+		return fmt.Sprintf("client IP = %s, port = %s", addrInfo[0], addrInfo[1])
+	case 4:
+		return fmt.Sprintf("requests served = %d", serverResponseCnt)
+	}
+	return ""
 }
