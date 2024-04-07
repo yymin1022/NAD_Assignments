bd0fa50b0e3a2bc676d35ab6eeb9e02a41a0850e
yymin1022 <yymin1022@gmail.com>
2024-04-08 04:42:27 +0900

    Update Assignment 2 : Added Basic Logic for Java TCP Client

--- Assignment 2/Java/EasyTCPClient.java
+++ Assignment 2/Java/EasyTCPClient.java
@@ -1,21 +1,114 @@
 import java.io.*;
-import java.net.Socket;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
 
 public class EasyTCPClient {
     static String SERVER_NAME = "localhost";
     static int SERVER_PORT = 14094;
 
-    public static void main(String[] args) throws IOException {
-        Socket serverConnection = new Socket(SERVER_NAME, SERVER_PORT);
+    public static void main(String[] args) {
+        Socket serverConnection = makeConnection();
+        if (serverConnection == null) {
+            printError("Failed to init client.");
+            return;
+        }
 
-        String msg= "Hello, World!";
-        PrintWriter writer = new PrintWriter(new OutputStreamWriter(serverConnection.getOutputStream()));
-        writer.println(msg);
-        writer.flush();
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> closeConnection(serverConnection)));
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(serverConnection.getInputStream()));
+        try {
+            serverConnection.setSoTimeout(5000);
+            while (true) {
+                printMenu();
 
-        writer.close();
-        reader.close();
+                int cmd = readCommand();
+                String text = "";
+                if (cmd == 0) {
+                    continue;
+                } else if (cmd == 5) {
+                    break;
+                } else if (cmd == 1) {
+                    System.out.print("Input lowercase sentence: ");
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                    text = reader.readLine();
+
+                    if (text.length() >= 1024) {
+                        printError("Text too long.");
+                        continue;
+                    }
+                }
+
+                String requestData = String.format("%d%s\r\n", cmd, text);
+
+                long requestTime = System.nanoTime();
+                OutputStream requestStream = serverConnection.getOutputStream();
+                requestStream.write(requestData.getBytes(StandardCharsets.UTF_8));
+
+                BufferedReader responseReader = new BufferedReader(new InputStreamReader(serverConnection.getInputStream()));
+                String responseData = responseReader.readLine();
+                long responseTime = System.nanoTime();
+
+                System.out.printf("\nReply from server: %s\n", responseData.trim());
+                System.out.printf("RTT = %.3fms\n", (responseTime - requestTime) / 1000000f);
+            }
+        } catch (SocketTimeoutException e) {
+            printError("Server Timeout.");
+        } catch (IOException e) {
+            printError("IO Error.");
+        }
+    }
+
+    private static Socket makeConnection() {
+        try {
+            return new Socket(SERVER_NAME, SERVER_PORT);
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    private static void closeConnection(Socket conn) {
+        System.out.println("\rClosing Client Program...\nBye bye~");
+        try {
+            conn.close();
+        } catch (IOException _) {}
+    }
+
+    private static void printMenu() {
+        System.out.println();
+        System.out.println("< Select Menu. >");
+        System.out.println("1) Convert Text to UPPER-case Letters");
+        System.out.println("2) Get Server Uptime");
+        System.out.println("3) Get Client IP / Port");
+        System.out.println("4) Get Count of Requests Server Got");
+        System.out.println("5) Exit Client");
+    }
+
+    private static int readCommand() {
+        int cmd;
+        String input;
+
+        try {
+            System.out.print("Input Command: ");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            input = reader.readLine();
+            cmd = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            printError("Invalid Command.");
+            return 0;
+        } catch (IOException e) {
+            printError("System Error.");
+            return 0;
+        }
+
+
+        if (cmd < 1 || cmd > 5) {
+            printError("Invalid Command.");
+            return 0;
+        }
+
+        return cmd;
+    }
+
+    private static void printError(String msg) {
+        System.err.printf("Error: %s\n", msg);
     }
 }
