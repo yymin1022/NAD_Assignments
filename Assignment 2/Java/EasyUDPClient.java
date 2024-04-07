255b222d9631d853aaf42bc57419f8e7b809722f
yymin1022 <yymin1022@gmail.com>
2024-04-07 21:50:49 +0900

    Update Assignment 2 : Added Basic Runtime Logic for Java UDP Client

--- Assignment 2/Java/EasyUDPClient.java
+++ Assignment 2/Java/EasyUDPClient.java
@@ -1,16 +1,107 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 
 public class EasyUDPClient {
     static String SERVER_NAME = "localhost";
     static int SERVER_PORT = 14094;
 
-    public static void main(String[] args) throws IOException {
-        DatagramSocket ds = new DatagramSocket();
+    public static void main(String[] args) {
+        DatagramSocket serverConnection = makeConnection();
+        if (serverConnection == null) {
+            printError("Failed to init client.");
+            return;
+        }
 
-        String msg = "Hello, World!";
-        DatagramPacket dp = new DatagramPacket(
-                msg.getBytes(), msg.getBytes().length, InetAddress.getByName(SERVER_NAME), SERVER_PORT);
-        ds.send(dp);
+        try {
+            while (true) {
+                printMenu();
+
+                int cmd = readCommand();
+                String text = "";
+                if (cmd == 0) {
+                    continue;
+                } else if (cmd == 5) {
+                    break;
+                } else if (cmd == 1) {
+                    System.out.print("Input lowercase sentence: ");
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                    text = reader.readLine();
+
+                    if(text.length() >= 1024){
+                        printError("Text too long.");
+                        continue;
+                    }
+                }
+
+                byte[] msgData = (cmd + text).getBytes(StandardCharsets.UTF_8);
+                DatagramPacket requestData = new DatagramPacket(
+                        msgData, msgData.length,
+                        InetAddress.getByName(SERVER_NAME), SERVER_PORT);
+                serverConnection.send(requestData);
+
+                byte[] responseBuffer = new byte[1024];
+                DatagramPacket responseData = new DatagramPacket(responseBuffer, responseBuffer.length);
+                serverConnection.receive(responseData);
+
+                System.out.printf("\nReply from server: %s\n", new String(responseData.getData()).trim());
+            }
+        } catch (IOException e) {
+            printError("IO Error.");
+            closeConnection(serverConnection);
+            return;
+        }
+
+        closeConnection(serverConnection);
+    }
+
+    private static DatagramSocket makeConnection() {
+        try{
+            return new DatagramSocket();
+        }catch (IOException e){
+            return null;
+        }
+    }
+
+    private static void closeConnection(DatagramSocket conn) {
+        conn.close();
+    }
+
+    private static void printMenu() {
+        System.out.println();
+        System.out.println("< Select Menu. >");
+        System.out.println("1) Convert Text to UPPER-case Letters");
+        System.out.println("2) Get Server Uptime");
+        System.out.println("3) Get Client IP / Port");
+        System.out.println("4) Get Count of Requests Server Got");
+        System.out.println("5) Exit Client");
+    }
+
+    private static int readCommand() {
+        String input;
+
+        try {
+            System.out.print("Input Command: ");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            input = reader.readLine();
+        } catch (NumberFormatException e) {
+            printError("Invalid Command.");
+            return 0;
+        } catch (IOException e) {
+            printError("System Error.");
+            return 0;
+        }
+
+        int cmd = Integer.parseInt(input);
+        if (cmd < 1 || cmd > 5) {
+            printError("Invalid Command.");
+            return 0;
+        }
+
+        return cmd;
+    }
+
+    private static void printError(String msg) {
+        System.err.printf("Error: %s\n", msg);
     }
 }
